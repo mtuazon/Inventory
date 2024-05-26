d66d14239a43e471f6d57dc48700bc9024f13ab9
AustineBill <auztine.malic@gmail.com>
2024-05-26 19:15:41 +0800

    hehehehe

---
+++ ims/inc/footer.php
@@ -0,0 +1,8 @@
+</head>
+
+<nav role="navigation" class="navbar navbar-dark  bg-primary bg-gradient">
+    <div class="container" >
+        <a class="navbar-brand">Inventory System</a>
+    </div>
+</nav>
+	
