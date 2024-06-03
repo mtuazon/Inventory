42f92527ce75df02ed9c4d8ad0a23e77d888a065
Maviii <144748980+mtuazon@users.noreply.github.com>
2024-06-03 20:41:40 +0800

    Update supplier.php
    icon, color

--- supplier.php
+++ supplier.php
@@ -13,19 +13,76 @@ $inventory->checkLogin();
 <script src="js/supplier.js"></script>
 <script src="js/common.js"></script>
 <?php include('inc/container.php');?>
-<div class="container">		
-		
-	<?php include("menus.php"); ?> 
+
+<style>
+html,
+body {
+    height: 100%;
+    background-color: #f0f0f0; /* Light gray background color */
+}
+
+.navbar-menu {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    background-color: #f0f0f0; /* Light gray background color */
+}
+
+.navbar-menu .nav-item {
+    margin: 0 10px;
+    padding: 10px;
+    background-color: #f0f0f0; /* Light gray background color */
+}
+
+.btn-primary {
+    background-color: #ff9900; /* Orange background color */
+    border-color: #ff9900; /* Orange border color */
+}
+
+.btn-primary:hover {
+    background-color: #e68a00; /* Darker orange on hover */
+    border-color: #e68a00; /* Darker orange on hover */
+}
+
+.btn-close {
+    color: #ff9900; /* Orange close button */
+}
+
+.card-header {
+    background-color: #ff9900; /* Orange background color */
+    color: white; /* White text color */
+}
+
+.table-striped tbody tr:nth-of-type(odd) {
+    background-color: rgba(255, 153, 0, 0.1); /* Light orange stripes */
+}
+
+.table-striped tbody tr:hover {
+    background-color: rgba(255, 153, 0, 0.3); /* Darker orange on hover */
+}
+
+.navbar-brand {
+    font-size: 24px;
+    font-weight: bold;
+    color: white;
+}
+</style>
+
+
+<div class="container-fluid">			
 	<div class="row">
-		<div class="col-lg-12">
+		<div class="col-lg-2">
+			<?php include("menus.php"); ?> 
+		</div>
+		<div class="col-lg-9" style="padding-top: 50px;">
 			<div class="card card-default rounded-0 shadow">
 				<div class="card-header">
 					<div class="row">
 						<div class="col-lg-10 col-md-10 col-sm-8 col-xs-6">
-							<h3 class="card-title">Supplier List</h3>
+						<h3 class="card-title" style="color: orange;"><i class="fas fa-truck"></i> Supplier List</h3>
 						</div>
 						<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 text-end">
-							<button type="button" name="add" id="addSupplier" data-bs-toggle="modal" data-bs-target="#userModal" class="btn btn-primary btn-sm rounded-0"><i class="far fa-plus-square"></i> Add Supplier</button>
+							<button type="button" name="add" id="addSupplier" data-bs-toggle="modal" data-bs-target="#userModal" class="btn btn-primary btn-sm rounded-0"><i class="fas fa-plus"></i> Add Supplier</button>
 						</div>
 					</div>					   
 					<div class="clear:both"></div>
@@ -54,29 +111,29 @@ $inventory->checkLogin();
         	<div class="modal-dialog modal-dialog-centered">
         			<div class="modal-content">
 						<div class="modal-header">
-							<h4 class="modal-title"><i class="fa fa-plus"></i> Add Supplier</h4>
-							<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+							<h4 class="modal-title"><i class="fas fa-plus"></i> Add Supplier</h4>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" style="color: orange;"></button>
 						</div>
 						<div class="modal-body">
         					<form method="post" id="supplierForm">
 								<input type="hidden" name="supplier_id" id="supplier_id" />
 								<input type="hidden" name="btn_action" id="btn_action" />
 								<div class="mb-3">
-									<label>Supplier Name</label>
+									<label for="supplier_name"><i class="fas fa-user"></i> Supplier Name</label>
 									<input type="text" name="supplier_name" id="supplier_name" class="form-control rounded-0" required />
 								</div>	
 								<div class="mb-3">
-									<label>Mobile</label>
+									<label for="mobile"><i class="fas fa-mobile-alt"></i> Mobile</label>
 									<input type="text" name="mobile" id="mobile" class="form-control rounded-0" required />
 								</div>								
 								<div class="mb-3">
-									<label>Address</label>
+									<label for="address"><i class="fas fa-address-card"></i> Address</label>
 									<textarea name="address" id="address" class="form-control rounded-0" rows="5" required></textarea>
 								</div>
         					</form>
 						</div>
 						<div class="modal-footer">
-							<input type="submit" name="action" id="action" class="btn btn-primary rounded-0 btn-sm" value="addSupplier" form="supplierForm"/>
+							<input type="submit" name="action" id="action" class="btn btn-primary rounded-0 btn-sm" value="Add Supplier" form="supplierForm"/>
 							<button type="button" class="btn btn-default border rounded-0 btn-sm" data-bs-dismiss="modal">Close</button>
 						</div>
 					</div>
@@ -84,4 +141,3 @@ $inventory->checkLogin();
         </div>	
 	</div>	
 </div>	
-<?php include('inc/footer.php');?>
